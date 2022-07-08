b1f0c646ce784be8ab8763a79e48221643e38076
rajaopak <ahmadnafis0302@gmail.com>
2022-07-08 22:23:24 +0700

    add time format

--- src/main/java/dev/rajaopak/opaklibs/libs/Common.java
+++ src/main/java/dev/rajaopak/opaklibs/libs/Common.java
@@ -211,4 +211,33 @@ public class Common {
     public static String getNmsVersion() {
         return nmsVersion;
     }
+
+    public String formatTime(int seconds) {
+        if (seconds == 0) {
+            return "0s";
+        }
+
+        long minute = seconds / 60;
+        seconds = seconds % 60;
+        long hour = minute / 60;
+        minute = minute % 60;
+        long day = hour / 24;
+        hour = hour % 24;
+
+        StringBuilder time = new StringBuilder();
+        if (day != 0) {
+            time.append(day).append("d ");
+        }
+        if (hour != 0) {
+            time.append(hour).append("h ");
+        }
+        if (minute != 0) {
+            time.append(minute).append("m ");
+        }
+        if (seconds != 0) {
+            time.append(seconds).append("s");
+        }
+
+        return time.toString().trim();
+    }
 }
